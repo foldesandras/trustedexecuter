41dcdbe0d96f99ae3957229d8893262083669127
Földes András <andras.foeldes14@gmail.com>
2025-03-29 19:41:06 +0100

    Add project files.

---
+++ texec/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace texec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdTxtBox = new System.Windows.Forms.TextBox();
+            this.modeComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdTxtBox
+            // 
+            this.cmdTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdTxtBox.Location = new System.Drawing.Point(15, 27);
+            this.cmdTxtBox.Name = "cmdTxtBox";
+            this.cmdTxtBox.Size = new System.Drawing.Size(439, 22);
+            this.cmdTxtBox.TabIndex = 0;
+            // 
+            // modeComboBox
+            // 
+            this.modeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modeComboBox.FormattingEnabled = true;
+            this.modeComboBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.modeComboBox.Items.AddRange(new object[] {
+            "Run normally",
+            "Run as Administrator",
+            "Run as \'NT AUTHORITY\\SYSTEM\'",
+            "Run as TrustedInstaller"});
+            this.modeComboBox.Location = new System.Drawing.Point(14, 82);
+            this.modeComboBox.Name = "modeComboBox";
+            this.modeComboBox.Size = new System.Drawing.Size(440, 24);
+            this.modeComboBox.TabIndex = 1;
+            this.modeComboBox.Text = "Run normally";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mode:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Run:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 188);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modeComboBox);
+            this.Controls.Add(this.cmdTxtBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrustedExecuter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cmdTxtBox;
+        private System.Windows.Forms.ComboBox modeComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ texec/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace texec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Run(string cmd, ProcessStartInfo customPsi = null)
+        {
+            // Alapértelmezett ProcessStartInfo a cmd.exe futtatásához
+            ProcessStartInfo defaultPsi = new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = "cmd.exe",
+                Arguments = $"/C start {cmd}"
+            };
+
+            // Ha van egy custom Psi, akkor az összes beállítást átmásoljuk
+            if (customPsi != null)
+            {
+                defaultPsi = customPsi;
+                // Ha a customPsi nem tartalmaz fájlnevet vagy parancsot, beállítjuk a default-ot
+                if (string.IsNullOrEmpty(customPsi.FileName))
+                {
+                    defaultPsi.FileName = "cmd.exe";
+                }
+                if (string.IsNullOrEmpty(customPsi.Arguments))
+                {
+                    defaultPsi.Arguments = $"/C start {cmd}";
+                }
+                defaultPsi.UseShellExecute = true;
+            }
+
+            // Elindítjuk a folyamatot a végleges ProcessStartInfo-val
+            Process.Start(defaultPsi);
+        }
+
+
+        private void RunNormally(string cmd)
+        {
+            Run(cmd);
+        }
+
+        private void RunAdmin(string cmd)
+        {
+            Run(cmd, new ProcessStartInfo() { Verb = "RunAs" });
+        }
+
+        private void RunTrustedInstaller(string cmd)
+        {
+            string ecmd = Convert.ToBase64String(Encoding.UTF8.GetBytes(cmd));
+            string payload = $"import-module ntobjectmanager;sc.exe start trustedinstaller;$p = get-ntprocess -name trustedinstaller.exe;$proc = new-win32process (\"cmd.exe /C start \" + [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(\"{ecmd}\"))) -creationflags newconsole -parentprocess $p;";
+            string epayload = Convert.ToBase64String(Encoding.Unicode.GetBytes(payload));
+            RunAdmin("powershell -EncodedCommand " + epayload);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cmd = cmdTxtBox.Text;
+            if (string.IsNullOrEmpty(cmd))
+            {
+                MessageBox.Show("Cannot run air!", "TrustedExecuter", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string mode = modeComboBox.Text;
+            switch (mode)
+            {
+                case "Run normally":
+                    RunNormally(cmd);
+                    break;
+                case "Run as Administrator":
+                    RunAdmin(cmd);
+                    break;
+                case "Run as TrustedInstaller":
+                    if (MessageBox.Show("Running stuff as TrustedInstaller is sketchy and if you do not know what you are doing, you could easily brick your system. Do you want to continue?", 
+                        "TrustedExecuter", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    {
+                        RunTrustedInstaller(cmd);
+                    }
+                    break;
+                default:
+                    MessageBox.Show($"Cannot run stuff in '{mode}' mode!", "TrustedExecuter", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+    }
+}
